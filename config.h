5d10e5fd6fe556534b68e14b4cdcdbb68f6278d1
ZhW <1065423410@qq.com>
2021-05-05 20:40:14 +0800

    dmenu replace rofi

--- config.h
+++ config.h
@@ -73,6 +73,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[] = { "rofi", "-show", "run", NULL};
 static const char *termcmd[]  = { "st", NULL };
 static const char *trayercmd[] = {"/home/lazywa/.dwm/scripts/t-toggle.sh"};
 
@@ -91,7 +92,7 @@ static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
     /* modifier                     key              function        argument */
-    { MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_p,            spawn,          {.v = roficmd } },
     { MODKEY|ShiftMask,             XK_Return,       spawn,          {.v = termcmd } },
     { MODKEY|ShiftMask,             XK_t,	         spawn,          {.v = trayercmd } },
     { MODKEY,                       XK_apostrophe,   togglescratch,  {.v = scratchpadcmd } },
